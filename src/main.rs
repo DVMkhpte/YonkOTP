851639654976993d12d0a3ed8d35b940d9d26e3b
DVM_khpte <epartelpro@gmail.com>
2025-03-18 17:06:49 +0100

    Add Modal "Add New Key"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, Builder, CssProvider};
+use gtk::{glib, Application, Builder, CssProvider, Window};
 use gtk::gdk::Display;
+use std::rc::Rc;
+use std::cell::RefCell;
 
 const APP_ID: &str = "org.yonkotp.main";
 const UI_FILE: &str = "resources/window.ui"; 
@@ -30,11 +32,59 @@ fn build_ui(app: &gtk::Application) {
 
     load_css();
     
-    if let Some(button) = builder.object::<gtk::Button>("ok_button") {
-        button.connect_clicked(|_| {
-            println!("Bouton cliqué !");
-        });
-    }
+     // Récupérer la fenêtre modale
+    let add_key_window: Window = builder
+    .object("add_key_window")
+    .expect("Échec du chargement de la modale");
+
+// Empêcher le redimensionnement et rendre la fenêtre modale
+add_key_window.set_transient_for(Some(&window)); // Associe la modale à la fenêtre principale
+add_key_window.set_modal(true);
+
+// Utilisation de Rc<RefCell<>> pour éviter les problèmes de propriété
+let add_key_window_rc = Rc::new(RefCell::new(add_key_window));
+
+// Récupérer le bouton "Add" et afficher la modale quand on clique dessus
+if let Some(button) = builder.object::<gtk::Button>("add_button") {
+    let add_key_window_clone = add_key_window_rc.clone();
+    button.connect_clicked(move |_| {
+        add_key_window_clone.borrow().set_visible(true);
+    });
+}
+
+// Bouton "Cancel" pour fermer la modale
+if let Some(cancel_button) = builder.object::<gtk::Button>("cancel_button") {
+    let add_key_window_clone = add_key_window_rc.clone();
+    cancel_button.connect_clicked(move |_| {
+        add_key_window_clone.borrow().set_visible(false);
+    });
+}
+
+// Bouton "Save" pour récupérer les inputs et fermer la modale
+if let Some(save_button) = builder.object::<gtk::Button>("save_button") {
+    let add_key_window_clone = add_key_window_rc.clone();
+    let service_entry = builder
+        .object::<gtk::Entry>("service_name_entry")
+        .expect("Champ service introuvable");
+    let username_mail_entry = builder
+        .object::<gtk::Entry>("username_mail_entry")
+        .expect("Champ username/mail introuvable");
+    let secret_entry = builder
+        .object::<gtk::Entry>("secret_key_entry")
+        .expect("Champ secret introuvable");
+
+    save_button.connect_clicked(move |_| {
+        let service_name = service_entry.text();
+        let secret_key = secret_entry.text();
+        let username_mail = username_mail_entry.text();
+
+        println!("Service: {}, Username/Mail: {}, Key: {}", service_name, username_mail, secret_key); // Debug output
+
+        // TODO: Envoyer vers la bdd
+
+        add_key_window_clone.borrow().set_visible(false);
+    });
+}
 
     window.present();
 }
@@ -63,9 +113,28 @@ fn load_css() {
         .green-button:hover {
             background: #218838;
         }
+        .otp-name {
+            font-size: 18px;
+            font-weight: normal;
+            padding-top: 5px;
+        }
         .otp-code {
-            font-size: 22px;
+            font-size: 30px;
             font-weight: bold;
+            padding-top: 5px;
+        }
+        .otp-timer {
+            font-size: 14px;
+            font-weight: bold;
+            color: #bbb; 
+        }
+        GtkListBoxRow {
+            background-color: #222;
+            border-radius: 5px;
+        }
+        GtkSeparator {
+            background-color: #555;
+            min-height: 1px;
         }
     ");
 
